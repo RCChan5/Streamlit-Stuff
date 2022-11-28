334edc6fb3b67fa2dad6e7a2ad56d752b9cc3633
Rosh C <43540251+RCChan5@users.noreply.github.com>
2022-11-28 18:06:27 -0500

    Add files via upload

--- pages/Know your worth.py
+++ pages/Know your worth.py
@@ -1,2 +1,47 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+from vega_datasets import data
+
+
+################################Data stuff##########################################
+df = pd.read_csv("data/Cleaned_Salaries.csv")
+#st.write(df)
+
+selection = df.iloc[:, [2,20,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58]]
+cols=["index","Job Title","Avg Salary(k)","Python","Spark","AWS","Excel","SQL","SAS","keras","pytorch","scikitlearn","TensorFlow","Tableau","PowerBI","Flink","MongoDB","Google An"]
+
+
+#st.write(selection)
+st.header("Salaries in Data Science")
+# options = st.multiselect(
+#     'What are your favorite colors',
+#     ['Green', 'Yellow', 'Red', 'Blue'],
+#     ['Yellow', 'Red'])
+#st.write('You selected:', options)
+st.write("Add your skill set and see where your salary is")
+
+options = st.multiselect(
+    'What tools do you know?',
+    ["Python","Spark","AWS","Excel","SQL","SAS","keras","pytorch","scikitlearn","TensorFlow","Tableau","PowerBI","Flink","MongoDB","Google An"],
+    ['Python'])
+
+
+
+
+#Figure 1 Salary vs title  Avg Salary(K)
+
+df2=df.loc[df["Job Title"].isin(options)]
+#st.write(df2)
+st.write("FIGURE 1: Average Salary")
+
+
+#st.write(df2)
+c1=alt.Chart(df2).mark_bar().encode(
+    alt.X("Avg Salary(K)",  bin=alt.Bin(maxbins=25)),
+    y='count()',
+)
+
+st.altair_chart(c1, use_container_width=True)
 
---
+++ test.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+from vega_datasets import data
+
+
+################################Data stuff##########################################
+df = pd.read_csv("data/Cleaned_Salaries.csv")
+job_title_option = df["Job Title"].unique()
+
+
+############################SIDE BAR################################################
+st.sidebar.success("Select a demo above.")
+
+#list of job titles to use in new df  ##should i keep this as a single value or multi
+option1 = st.sidebar.multiselect(
+    'Figure1: What job are you interested in?',
+    job_title_option,["Data Scientist"])
+
+#testing
+
+
+
+
+###################################MAIN PAGE######################################
+
+st.header("Salaries in Data Science")
+# options = st.multiselect(
+#     'What are your favorite colors',
+#     ['Green', 'Yellow', 'Red', 'Blue'],
+#     ['Yellow', 'Red'])
+#st.write('You selected:', options)
+st.write("Add your skill set and see where your salary is")
+
+
+
+#Figure 1 Salary vs title  Avg Salary(K)
+
+df2=df.loc[df["Job Title"].isin(option1)]
+#st.write(df2)
+st.write("FIGURE 1: Average Salary")
+
+
+#st.write(df2)
+c1=alt.Chart(df2).mark_bar().encode(
+    alt.X("Avg Salary(K)",  bin=alt.Bin(maxbins=25)),
+    y='count()',
+)
+
+st.altair_chart(c1, use_container_width=True)
+
+## Figure 2
+
+st.write("FIGURE 2: What type of Degrees are used in the field?")
+
+
+#st.write(df2)
+c2=alt.Chart(df2).mark_bar().encode(
+    x="Degree",
+    y='count()',
+)
+
+st.altair_chart(c2, use_container_width=True)
+
+## Figure 3
+
+st.write("FIGURE 3: Senority level")
+
+
+#st.write(df2)
+c3=alt.Chart(df2).mark_bar().encode(
+    x="seniority_by_title",
+    y='count()',
+)
+
+st.altair_chart(c3, use_container_width=True)
+
+
+
+
+
+
+
+
