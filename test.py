9dbd356d217ced7ccefbf8dc349bf5d0b381e6ea
Rosh C <43540251+RCChan5@users.noreply.github.com>
2022-11-28 18:39:31 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
-from vega_datasets import data
+
 
 
 ################################Data stuff##########################################
